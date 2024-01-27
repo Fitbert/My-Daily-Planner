5b6aab1882786e6833790de9bd73ea018521951c
Albert T <albert.terrazas@gmail.com>
2024-01-27 11:13:14 -0600

    added features
    added event listener and time

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
+var saveBtnEl = $('#saveBtn');
+var timeBlockEl =$('#time-block');
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+$(document).ready(function() {
+  var dateFormat = moment().format("MMMM Do YYYY");
+  let curDate = document.getElementById("currentDay");
+  curDate.innerHTML = dateFormat;
+  var a = moment().format("HH");
+  $("#clearFieldsBtn").click(function(dateFormat) {
+      dateFormat.preventDefault,
+      $("textarea").val(""),
+      localStorage.clear()
+  });
+  $(".time-div").each(function() {
+      var t = $(this).attr("id").split("-")[1];
+      a == t ? ($(this).addClass("present"),
+      $(this).children(".description").addClass("white-text")) : a < t ? ($(this).removeClass("present"),
+      $(this).addClass("future")) : t < a && ($(this).removeClass("future"),
+      $(this).addClass("past"))
+  });
+});
 
-var saveBtnEl = $('#saveBtn');
-var timeBlockEl =$('#time-block');
 
 
 $(function () {
   saveBtnEl.on('click', function(){
     alert('ive been clicked');
-  });
+  })});
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -29,4 +47,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+  
